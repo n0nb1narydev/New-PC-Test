143b92d2d17d2f7066cf17c996c19ab79954a7be
Mel Rumsey <melsrumsey@gmail.com>
2020-09-09 09:19:44 -0500

    complete

--- media.cs
+++ media.cs
@@ -7,6 +7,10 @@ namespace Treehouse.MediaLibrary
         static void Main()
         {
             var book = new Books("The Way of Kings", "Brandon Sanderson");
+            var movie = new Movies("Carol", "R");
+            var song = new Music("Some Beach", "Blake Shelton");
+
+            Console.WriteLine($"Book: {book}")
         }
     }
 }
